490248bdabfc3992682c7bc94d98ecb7e6bc99bb
sudha123 <sudhanaukala1997@gmail.com>
2023-04-07 21:23:32 +0530

    Simple-Web-Server

---
+++ src/main/java/SimpleHttpServer.java
@@ -0,0 +1,31 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class SimpleHttpServer {
+    public static int DEFAULT_PORT = 9000;
+    public static int port;
+    private HttpServer httpServer;
+
+    private void start(int port) {
+        this.port = port;
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at " + port);
+            httpServer.createContext("/", new Handlers.RootHandler());
+            httpServer.createContext("/echoHeader", new Handlers.EchoHeaderHandler());
+            httpServer.createContext("/echoGET", new Handlers.EchoGetHandler());
+            httpServer.createContext("/echoPost", new Handlers.EchoPostHandler());
+            httpServer.setExecutor(null);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleHttpServer httpServer = new SimpleHttpServer();
+        httpServer.start(DEFAULT_PORT);
+    }
+}
